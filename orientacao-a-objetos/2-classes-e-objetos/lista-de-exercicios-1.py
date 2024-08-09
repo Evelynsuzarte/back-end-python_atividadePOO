f833ac9d145405eebe9f951673a87fdc08b62fb1
Evelynsuzarte <evelynsuzarte@hotmail.com>
2024-08-09 20:54:12 -0300

    exercício 1

--- orientacao-a-objetos/2-classes-e-objetos/lista-de-exercicios-1.py
+++ orientacao-a-objetos/2-classes-e-objetos/lista-de-exercicios-1.py
@@ -11,3 +11,39 @@
 
 
 # Faça o carro "parar" utilizando os métodos da sua classe.
+
+
+class Carro:
+    def __init__(self):
+        self.ligado = False
+        self.cor = ""
+        self.modelo = ""
+        self.velocidade = 0
+
+    def liga(self):
+        self.ligado = True
+    
+    def desliga(self):
+        self.ligado = False
+        self.velocidade = 0
+    
+    def acelera(self, km):
+        self.velocidade = self.velocidade+km
+
+    def desacelera(self, km):
+        self.velocidade = self.velocidade-km
+
+    def __str__(self) -> str:
+        return f'Carro - ligado {self.ligado} - cor {self.cor} - modelo {self.modelo} - velocidade {self.velocidade}'
+
+
+carro_1 = Carro()
+carro_1.liga()
+carro_1.acelera(16)
+print(carro_1)
+carro_1.acelera(30)
+print(carro_1)
+carro_1.desacelera(10)
+print(carro_1)
+carro_1.desliga()
+print(carro_1)
